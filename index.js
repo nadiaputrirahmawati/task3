a1b3487eb57721fc12bdee0b91185070c2902c93
nadiaputrirahmawati <nadiaputrirahman@gmail.com>
2024-09-30 21:19:24 +0700

    nad

---
+++ index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form-demo");
+  const text = document.getElementById("text_info");
+  const tombolTema = document.getElementById("tema");
+  const bodyElement = document.body;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("nama").value;
+    const skill = document.getElementById("skill").value;
+
+    text.textContent = `Hello, ${name}! ${skill} Yang kamu pilih .`;
+
+    const colors = {
+      HTML: "#e74c3c",
+      CSS: "#3498db",
+      JAVASCRIPT: "#f1c40f",
+    };
+
+    document.documentElement.style.setProperty("--blue-color",colors[skill] || "#000");
+  });
+
+  tombolTema.addEventListener("click", () => {
+    bodyElement.classList.toggle("dark-mode");
+
+    if (bodyElement.classList.contains("dark-mode")) {
+      themeButton.textContent = "Light Mode";
+    } else {
+      themeButton.textContent = "Dark Mode";
+    }
+  });
+});
